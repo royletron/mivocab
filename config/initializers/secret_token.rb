a8656d1c53c46e1f123eabcf698cb014d3148449
Darren <darren.royle@oup.com>
2013-02-22 14:33:36 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mivocab::Application.config.secret_token = '8d474a447a36f9144be93d64675c333ba33b346a27580cb4bccc33f5aa2ef17e30aa4149e31fd76071236d0b4fa48e435d18426c2f4d0e7694917d7d1d3f02b8'
